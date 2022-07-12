8320b52fc85f434d0e550079b5da1a890ff3b3d1
JakeOxide <101085109+JakeOxide@users.noreply.github.com>
2022-07-12 19:44:43 +0300

    Shop App : Initial Layout and Entry Validation
    Shop App : Initial Layout and Entry Validation included. Needs a validation class and more OOP applied.

---
+++ Item_counter_GUI/ShopApp/ShopApp/Form1.Designer.cs
@@ -0,0 +1,258 @@
+﻿namespace ShopApp
+{
+    partial class ShopApp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel_main = new System.Windows.Forms.Panel();
+            this.txt_itemName = new System.Windows.Forms.TextBox();
+            this.txt_itemCount = new System.Windows.Forms.TextBox();
+            this.txt_itemPrice = new System.Windows.Forms.TextBox();
+            this.txt_inventoryNumber = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.txt_entryNumber = new System.Windows.Forms.TextBox();
+            this.btn_cancel = new System.Windows.Forms.Button();
+            this.btn_clear = new System.Windows.Forms.Button();
+            this.btn_add = new System.Windows.Forms.Button();
+            this.lbl_price = new System.Windows.Forms.Label();
+            this.lbl_itemCount = new System.Windows.Forms.Label();
+            this.lbl_itemName = new System.Windows.Forms.Label();
+            this.lbl_inventoryNumber = new System.Windows.Forms.Label();
+            this.lbl_date = new System.Windows.Forms.Label();
+            this.lbl_entryNumber = new System.Windows.Forms.Label();
+            this.panel_main.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel_main
+            // 
+            this.panel_main.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.panel_main.Controls.Add(this.txt_itemName);
+            this.panel_main.Controls.Add(this.txt_itemCount);
+            this.panel_main.Controls.Add(this.txt_itemPrice);
+            this.panel_main.Controls.Add(this.txt_inventoryNumber);
+            this.panel_main.Controls.Add(this.dateTimePicker1);
+            this.panel_main.Controls.Add(this.txt_entryNumber);
+            this.panel_main.Controls.Add(this.btn_cancel);
+            this.panel_main.Controls.Add(this.btn_clear);
+            this.panel_main.Controls.Add(this.btn_add);
+            this.panel_main.Controls.Add(this.lbl_price);
+            this.panel_main.Controls.Add(this.lbl_itemCount);
+            this.panel_main.Controls.Add(this.lbl_itemName);
+            this.panel_main.Controls.Add(this.lbl_inventoryNumber);
+            this.panel_main.Controls.Add(this.lbl_date);
+            this.panel_main.Controls.Add(this.lbl_entryNumber);
+            this.panel_main.Location = new System.Drawing.Point(12, 16);
+            this.panel_main.Name = "panel_main";
+            this.panel_main.Size = new System.Drawing.Size(776, 422);
+            this.panel_main.TabIndex = 0;
+            // 
+            // txt_itemName
+            // 
+            this.txt_itemName.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.txt_itemName.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_itemName.Location = new System.Drawing.Point(48, 174);
+            this.txt_itemName.Name = "txt_itemName";
+            this.txt_itemName.Size = new System.Drawing.Size(402, 24);
+            this.txt_itemName.TabIndex = 15;
+            // 
+            // txt_itemCount
+            // 
+            this.txt_itemCount.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.txt_itemCount.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_itemCount.Location = new System.Drawing.Point(48, 267);
+            this.txt_itemCount.Name = "txt_itemCount";
+            this.txt_itemCount.Size = new System.Drawing.Size(122, 24);
+            this.txt_itemCount.TabIndex = 14;
+            // 
+            // txt_itemPrice
+            // 
+            this.txt_itemPrice.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.txt_itemPrice.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_itemPrice.Location = new System.Drawing.Point(250, 267);
+            this.txt_itemPrice.Name = "txt_itemPrice";
+            this.txt_itemPrice.Size = new System.Drawing.Size(99, 24);
+            this.txt_itemPrice.TabIndex = 13;
+            // 
+            // txt_inventoryNumber
+            // 
+            this.txt_inventoryNumber.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.txt_inventoryNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_inventoryNumber.Location = new System.Drawing.Point(492, 79);
+            this.txt_inventoryNumber.Name = "txt_inventoryNumber";
+            this.txt_inventoryNumber.Size = new System.Drawing.Size(193, 24);
+            this.txt_inventoryNumber.TabIndex = 11;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.CalendarFont = new System.Drawing.Font("Arial Narrow", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePicker1.Font = new System.Drawing.Font("Arial Narrow", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePicker1.Location = new System.Drawing.Point(250, 79);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 21);
+            this.dateTimePicker1.TabIndex = 10;
+            // 
+            // txt_entryNumber
+            // 
+            this.txt_entryNumber.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.txt_entryNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_entryNumber.Location = new System.Drawing.Point(48, 79);
+            this.txt_entryNumber.Name = "txt_entryNumber";
+            this.txt_entryNumber.Size = new System.Drawing.Size(151, 24);
+            this.txt_entryNumber.TabIndex = 9;
+            // 
+            // btn_cancel
+            // 
+            this.btn_cancel.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btn_cancel.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_cancel.Location = new System.Drawing.Point(451, 347);
+            this.btn_cancel.Name = "btn_cancel";
+            this.btn_cancel.Size = new System.Drawing.Size(172, 31);
+            this.btn_cancel.TabIndex = 8;
+            this.btn_cancel.Text = "Cancel";
+            this.btn_cancel.UseVisualStyleBackColor = false;
+            // 
+            // btn_clear
+            // 
+            this.btn_clear.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btn_clear.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_clear.Location = new System.Drawing.Point(250, 347);
+            this.btn_clear.Name = "btn_clear";
+            this.btn_clear.Size = new System.Drawing.Size(172, 31);
+            this.btn_clear.TabIndex = 7;
+            this.btn_clear.Text = "Clear Screen";
+            this.btn_clear.UseVisualStyleBackColor = false;
+            // 
+            // btn_add
+            // 
+            this.btn_add.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btn_add.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_add.Location = new System.Drawing.Point(46, 347);
+            this.btn_add.Name = "btn_add";
+            this.btn_add.Size = new System.Drawing.Size(172, 31);
+            this.btn_add.TabIndex = 6;
+            this.btn_add.Text = "Add to Inventory";
+            this.btn_add.UseVisualStyleBackColor = false;
+            this.btn_add.Click += new System.EventHandler(this.btn_add_Click);
+            // 
+            // lbl_price
+            // 
+            this.lbl_price.AutoSize = true;
+            this.lbl_price.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_price.Location = new System.Drawing.Point(245, 225);
+            this.lbl_price.Name = "lbl_price";
+            this.lbl_price.Size = new System.Drawing.Size(121, 30);
+            this.lbl_price.TabIndex = 5;
+            this.lbl_price.Text = "Item Price";
+            // 
+            // lbl_itemCount
+            // 
+            this.lbl_itemCount.AutoSize = true;
+            this.lbl_itemCount.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_itemCount.Location = new System.Drawing.Point(43, 225);
+            this.lbl_itemCount.Name = "lbl_itemCount";
+            this.lbl_itemCount.Size = new System.Drawing.Size(127, 30);
+            this.lbl_itemCount.TabIndex = 4;
+            this.lbl_itemCount.Text = "Item Count";
+            // 
+            // lbl_itemName
+            // 
+            this.lbl_itemName.AutoSize = true;
+            this.lbl_itemName.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_itemName.Location = new System.Drawing.Point(43, 130);
+            this.lbl_itemName.Name = "lbl_itemName";
+            this.lbl_itemName.Size = new System.Drawing.Size(128, 30);
+            this.lbl_itemName.TabIndex = 3;
+            this.lbl_itemName.Text = "Item Name";
+            // 
+            // lbl_inventoryNumber
+            // 
+            this.lbl_inventoryNumber.AutoSize = true;
+            this.lbl_inventoryNumber.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_inventoryNumber.Location = new System.Drawing.Point(487, 35);
+            this.lbl_inventoryNumber.Name = "lbl_inventoryNumber";
+            this.lbl_inventoryNumber.Size = new System.Drawing.Size(198, 30);
+            this.lbl_inventoryNumber.TabIndex = 2;
+            this.lbl_inventoryNumber.Text = "Inventory Number";
+            // 
+            // lbl_date
+            // 
+            this.lbl_date.AutoSize = true;
+            this.lbl_date.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_date.Location = new System.Drawing.Point(247, 35);
+            this.lbl_date.Name = "lbl_date";
+            this.lbl_date.Size = new System.Drawing.Size(62, 30);
+            this.lbl_date.TabIndex = 1;
+            this.lbl_date.Text = "Date";
+            // 
+            // lbl_entryNumber
+            // 
+            this.lbl_entryNumber.AutoSize = true;
+            this.lbl_entryNumber.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_entryNumber.Location = new System.Drawing.Point(43, 35);
+            this.lbl_entryNumber.Name = "lbl_entryNumber";
+            this.lbl_entryNumber.Size = new System.Drawing.Size(156, 30);
+            this.lbl_entryNumber.TabIndex = 0;
+            this.lbl_entryNumber.Text = "Entry Number";
+            // 
+            // ShopApp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel_main);
+            this.MaximumSize = new System.Drawing.Size(816, 489);
+            this.MinimumSize = new System.Drawing.Size(816, 489);
+            this.Name = "ShopApp";
+            this.Text = "Add Inventory";
+            this.panel_main.ResumeLayout(false);
+            this.panel_main.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel_main;
+        private System.Windows.Forms.TextBox txt_itemName;
+        private System.Windows.Forms.TextBox txt_itemCount;
+        private System.Windows.Forms.TextBox txt_itemPrice;
+        private System.Windows.Forms.TextBox txt_inventoryNumber;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.TextBox txt_entryNumber;
+        private System.Windows.Forms.Button btn_cancel;
+        private System.Windows.Forms.Button btn_clear;
+        private System.Windows.Forms.Button btn_add;
+        private System.Windows.Forms.Label lbl_price;
+        private System.Windows.Forms.Label lbl_itemCount;
+        private System.Windows.Forms.Label lbl_itemName;
+        private System.Windows.Forms.Label lbl_inventoryNumber;
+        private System.Windows.Forms.Label lbl_date;
+        private System.Windows.Forms.Label lbl_entryNumber;
+    }
+}
+
---
+++ Item_counter_GUI/ShopApp/ShopApp/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ShopApp
+{
+    public partial class ShopApp : Form
+    {
+        public ShopApp()
+        {
+            InitializeComponent();
+        }
+
+        private bool checkBoxes()
+        {
+            bool charCount = false;
+
+            if (txt_entryNumber.Text.Length > 0)
+                charCount = true;
+            else
+                return false;
+
+            if (txt_inventoryNumber.Text.Length > 0)
+                charCount = true;
+            else
+                return false;
+
+            if (txt_itemCount.Text.Length > 0)
+                charCount = true;
+            else
+                return false;
+
+            if (txt_itemName.Text.Length > 0)
+                charCount = true;
+            else
+                return false;
+
+            if (txt_itemPrice.Text.Length > 0)
+                charCount = true;
+            else
+                return false;
+
+            return charCount;
+        }
+
+        private void btn_add_Click(object sender, EventArgs e)
+        {
+            if (checkBoxes())
+            {
+                DataClass data = new DataClass();
+                data.itemName = txt_itemName.Text;
+                data.itemNumber = txt_entryNumber.Text;
+                bool check = data.validateData();
+                if (check)
+                {
+                    MessageBox.Show("This is a valid entry " + txt_entryNumber.Text);
+                }
+                else
+                {
+                    MessageBox.Show("Invalid Entry " + txt_entryNumber.Text);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please fill the empty fields");
+            }
+        }
+    }
+
+    class DataClass
+    {
+        public string itemName { get; set; }
+        public string itemNumber { get; set; }
+        public string  inventoryNumber { get; set; }
+        public string itemCount { get; set; }
+        public double itemPrice { get; set; }
+
+        public bool validateData()
+        {
+            bool result = false;
+
+            // Name
+            char[] nameChar = itemName.Trim().ToCharArray();
+            if (nameChar.Length > 2)
+            {
+                foreach (char x in nameChar)
+                {
+                    if (x >= 'a' & x <= 'z' || x >= 'A' & x <= 'Z')
+                    {
+                        result = true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            //Number
+            int[] itemNum = itemNumber.ToString().ToCharArray().Select(Convert.ToInt32).ToArray();
+            bool checker = false;
+                foreach (int x in itemNum)
+                    {
+                        if (x >= 0 | x <= 9)
+                        {
+                            result = true;
+                            checker = true;
+                        }
+                        else
+                        {
+                            checker = false;
+                            return false;
+                        }
+                    }
+            if (checker)
+            {
+                int entryNum = Convert.ToInt32(itemNumber);
+                if (entryNum >= 1 && entryNum <= 1000)
+                {
+                    result = true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+
+            return result;
+        }
+
+    }
+
+}
